784588db4ab69683f8c31d01aed761781311ce70
Laurent Van Acker <laurentvanacker@gmail.com>
2021-12-19 18:28:24 +0100

    updated radio with legit graphics

--- src/egg/Radio.gd
+++ src/egg/Radio.gd
@@ -5,6 +5,10 @@ onready var previous_button = $PreviousButton
 onready var power_button = $PowerButton
 
 
+onready var states = [
+	$S1, $S2, $S3
+]
+
 var enabled = false
 var songs = ["classical", "jazz", "metal"]
 var current_index = 0
@@ -12,12 +16,13 @@ var current_index = 0
 var timer = 0.0
 
 func _ready():
-	pass
+	deactivate()
 
 func _process(delta):
 	timer += delta
 
 func toggle_power():
+	press_button($PowerButton)
 	if enabled:
 		deactivate()
 	else:
@@ -30,6 +35,27 @@ func deactivate():
 	enabled = false
 	AudioEngine.stop_background_music()
 	GlobalData.music = null
+	update_lights()
+
+func update_lights():
+	if enabled:
+		$OnOff.show()
+	else:
+		$OnOff.hide()
+
+	for i in range(states.size()):
+		if enabled:
+			if i == current_index:
+				states[i].show()
+			else:
+				states[i].hide()
+		else:
+			states[i].hide()
+
+func press_button(button: ClickableTexture):
+	button.z_index = 2
+	yield(get_tree().create_timer(0.4), "timeout")
+	button.z_index = -1
 
 func play():
 	print("Current index ", current_index)
@@ -39,18 +65,23 @@ func play():
 func activate():
 	enabled = true
 	play()
+	update_lights()
 
 func next_song():
+	press_button($NextButton)
 	if not enabled:
 		return
 	current_index = (current_index + 1) % songs.size()
 	play()
+	update_lights()
 
 func previous_song():
+	press_button($PreviousButton)
 	if not enabled:
 		return
 	current_index = (current_index - 1) % songs.size()
 	if current_index < 0:
 		current_index += songs.size()
 	play()
+	update_lights()
 
